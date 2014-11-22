1d5b270ca4928d39128cc61de0f6321e693b0ce7
emily_borges <emily_borges@10.1.48.138>
2014-11-22 17:40:34 -0200

    A

--- src/test/Test.java
+++ src/test/Test.java
@@ -10,10 +10,5 @@ package test;
  */
 public class Test {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+   private String nome;
 }
